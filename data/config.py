775d2ef10dcc39c180325cb86cbe296f2d38a4c1
Doston <isoyevdoston1@gmail.com>
2023-07-30 00:37:04 +0500

    updated db host

--- data/config.py
+++ data/config.py
@@ -1,16 +1,16 @@
 from environs import Env
-
+import os
 # environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+ADMINS = list(os.environ.get("ADMINS"))
 
 CHANNELS = ["@Portifolio_Doston","-1001596165960"]
 
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+DB_USER = str(os.environ.get("DB_USER"))
+DB_PASS = str(os.environ.get("DB_PASS"))
+DB_NAME = str(os.environ.get("DB_NAME"))
+DB_HOST = str(os.environ.get("DB_HOST"))
